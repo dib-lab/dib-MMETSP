bf553374969edfdcd3578532d322a257daa68175
Lisa Cohen <ljcohen@ucdavis.edu>
2017-01-15 17:43:07 -0800

    Update dibMMETSP_configuration.py

--- dibMMETSP_configuration.py
+++ dibMMETSP_configuration.py
@@ -2,8 +2,8 @@
 # Simplicity at its best, my friends: http://stackoverflow.com/questions/8225954/python-configuration-file-any-file-format-recommendation-ini-format-still-appr
 
 # Base directory where scripts are run and files will be stored
-#base_dir = '/mnt/home/ljcohen/'
-base_dir = '/Users/cohenl06/Documents/UCDavis/dib/MMETSP/test/'
+base_dir = '/mnt/home/ljcohen/'
+
 # CSV file containing the MMETSP ids and SRA ids for the samples you want to run
 sra_csv = 'SraRunInfo.csv'
 
@@ -11,16 +11,16 @@ sra_csv = 'SraRunInfo.csv'
 assembly_version = 'assembly_trinity_test.py'
 
 # Directory where scripts will be written within base_dir
-scripts_dir = '/Users/cohenl06/Documents/UCDavis/dib/MMETSP/dib-MMETSP'
+scripts_dir = '/mnt/home/ljcohen/MMETSP/'
 
 # Directory where final output will be stored within base_dir
-output_dir = '/Users/cohenl06/Documents/UCDavis/dib/MMETSP/test/assembly_trinity_2.2.0/'
+output_dir = '/mnt/home/ljcohen/assembly_trinity_2.2.0/'
 
 # Extension added to the end of the output files
 output_extension = '.trinity_out_2.2.0'
 
 # Extension for Trinity
 output_trinity_extension = '.Trinity.fasta'
+
 # Directory where intermediate files will be stored
-#data_dir = '/mnt/scratch/ljcohen/mmetsp/'
-data_dir = '/Users/cohenl06/Documents/UCDavis/dib/MMETSP/test_temp/'
+data_dir = '/mnt/scratch/ljcohen/mmetsp/'
